178ea06fc798737a292670fc4529307becc8fbb5
Stefan Allen <Stefanallen10@gmail.com>
2024-11-25 01:15:53 +0000

    Image Converter
    + Image converter

---
+++ src/app/page.tsx
@@ -0,0 +1,35 @@
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <div className={styles.ctas}>
+                    <Link
+                        href="/converter/imageconverter"
+                        passHref
+                    >
+                        <div className={styles.ctaButton}>
+                            Image Converter
+                        </div>
+                    </Link>
+                </div>
+
+                <div className={styles.logo}>
+                    File Converter
+                </div>
+
+                <ol className={styles.instructions}>
+                    <li>
+                        Get started by selecting <code>Converter</code>.
+                    </li>
+                    <li>
+                        Instantly convert files for <code>free</code> (images, audio, video, archive files).
+                    </li>
+
+                </ol>
+            </main>
+        </div>
+    );
+}
